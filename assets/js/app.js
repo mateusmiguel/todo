62b363970bc17c4c91ad66317d9b894121a198ae
Mateus Miguel <webmateusmiguel@gmail.com>
2020-07-27 12:26:36 -0300

    Reder todos

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,8 @@
 const items = document.querySelector(".items");
 const submitForm = document.querySelector(".add");
 const submitButton = document.querySelector(".add-btn");
-// const list = document.querySelector('.items .item') !== null ? document.querySelector('.items .item') : '0';
-// let listLength = list.lenght ?;
-// console.log(list);
+
+let todos = ["Making coffe", "Study JS", "Take off the trash"];
 
 function addTodo(e) {
   console.log('add Task')
@@ -17,45 +16,19 @@ function tickTodo(e) {
   console.log('tick Task');
 }
 
-function renderTodos(e) {
-  console.log('render Tasks')
+function renderTodos() {
+  todos.map((todo, id) => {
+
+    let todoElement = `
+      <div class="item">
+        <input type="checkbox" name="item-${id}" id="item-${id}">
+        <label for="item-1">${todo}</label>
+        <i class="far fa-trash-alt delete-btn"></i>
+      </div>`;
+    console.log(todoElement)
+    items.innerHTML += todoElement;
+
+  })
 }
 
-// let todos = ["Making coffe", "Study JS", "Take off the trash"];
-
-// function renderTodos() {
-//   for (todo of todos) {
-//     var todoElement = document.createElement("div");
-//     var todoText = document.createTextNode(todo);
-
-//     todoElement.appendChild(todoText);
-//     items.appendChild(todoElement);
-//   }
-// }
-
-// const generateTemplate = (todo) => {
-  // const html = `<li>
-  //                 <input type="checkbox" id="todo_${listLength}" />
-  //                 <label for="todo_${listLength}">
-  //                     <span class="check"></span>${todo}
-  //                 </label>
-  //                 <i class="far fa-trash-alt delete"></i>
-  //               </li>`;
-
-//   const html = `
-//     <div class="item">
-//       <input type="checkbox" name="item-${listLenght}" id="item-{listLenght}">
-//       <label for="item-1">${todo}</label>
-//       <i class="far fa-trash-alt delete-btn"></i>
-//     </div>`;
-
-//   todoList.innerHTML += html;
-// };
-
-// renderTodos();
-// 
-// <div class="item">
-//   <input type="checkbox" name="item-1" id="item-1">
-//   <label for="item-1">Checkout email</label>
-//   <i class="far fa-trash-alt delete-btn"></i>
-// </div>
+renderTodos();
